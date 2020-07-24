50f62bd5c05f513e52e21d59a67ddda75a9cb077
Chao <c.xu-6@student.tudelft.nl>
2020-07-24 17:28:44 +0800

    XDJT finish

--- main.cpp
+++ main.cpp
@@ -64,9 +64,9 @@ void Keccak_frho(uint64_t input[5][5], uint64_t output[5][5])
         {28, 55, 25, 21, 56},
         {27, 20, 39, 8, 14}
 	};
-    for (int i = 0; i < MAT; ++i) 
+    for (int i = 0; i < 5; ++i) 
 	{
-        for (int j = 0; j < MAT; ++j) 
+        for (int j = 0; j < 5; ++j) 
 		{
             output[i][j] = Keccak_frot(input[i][j], Rho_rot[i][j]);
         }
@@ -139,7 +139,7 @@ void Keccak_fiota(uint64_t input[5][5], int round)
 	//Input: @input 5*5 matrix of uint64_t, which store the matrix before chi transformation
 	//       @round the number of round it is in
 	// Do xor to input[0][0] avoid symmetricity,each round the xor number is not the same
-	const uint64_t iota[5][5]=
+	const uint64_t iota[]=
 	{
 		0x0000000000000001, 0x0000000000008082, 0x800000000000808A, 0x8000000080008000,
         0x000000000000808B, 0x0000000080000001, 0x8000000080008081, 0x8000000000008009, 
@@ -176,11 +176,142 @@ void Keccak_f(uint64_t input[5][5], uint64_t output[5][5],int round)
     Keccak_fiota(buf_2, round);
     Keccak_cpy(buf_2,output);
 }
+
+
+
+
+uint64_t* encrypt(int type, uint64_t * seq, int seq_length)
+{
+	//Input: @type: define the type of Keccak
+	//       @seq: the array of uint8_t(but stored in uint64_t, the former 56 bits should alwways be 0),
+	//       because when we padding, we padding uint8_t. Stored as uint64_t avoid type changing
+	//       And because each input char is composed of 8bits, so input should be uint8_t 
+	//       @seq_length: The number of uint8_t in the array
+	//Output: the array of uint8_t(but stored in uint64_t, the former 56 bits should alwways be 0)
+	int i,j,k;
+	const int b=200;
+	int output_length=type/8;
+    int r, c; //in Byte
+    if (type == 224) 
+	{
+        c = 56;
+    } 
+	else if (type == 256) 
+	{
+        c = 64;
+    } 
+	else if (type == 384) 
+	{
+        c = 96;
+    } 
+	else if (type == 512) 
+	{
+        c = 128;
+    } 
+	else 
+	{
+		
+		cout<<"No type"<<endl;
+        return NULL;
+    }
+    
+    r=b-c;
+        
+        
+        
+    
+    int zero = 0;//the number of 0x00 at the tail
+    if ((seq_length + 1)%r!=0) {
+        zero=r-(seq_length+1)%r;//1 for 0x0000000000000006
+    }
+    int block = (seq_length+1+zero)/r;//How many blocks of length r
+    uint64_t* padded_seq = new uint64_t[seq_length+1+zero];// the finally added sequence
+    for(i=0;i<seq_length;i++)
+    {
+    	padded_seq[i]=seq[i];
+	}
+    
+    padded_seq[seq_length]=0x0000000000000006;
+    for(i=0;i<zero;i++)
+    {
+    	padded_seq[i+seq_length+1]=0x0000000000000000;
+	}
+    padded_seq[seq_length+zero]=padded_seq[seq_length+zero]|0x0000000000000080;   
+    
+    
+    
+    
+    uint64_t output[5][5];
+    
+    
+    
+    
+    for(i=0;i<5;i++)
+    {
+    	for(j=0;j<5;j++)
+    	{
+    		output[i][j]=0x0000000000000000;
+		}
+	}
+    
+    
+    
+    
+    
+
+    for (i = 0; i < block; ++i) 
+	{
+        int position = 0;
+        //totally the bumber of block X absorbed 
+        for (j=i*r; j<(i+1)*r;j=j+8) 
+		{
+			//j=j+8 because 1 uint_64 is composed of 8 uint_8
+			// we need to reget the number in uint_64 from uint_8
+            uint64_t t = 0x0000000000000000;
+            for (k=j+7;k>=j;--k) 
+			{
+				t=t<<8;
+                t =t|padded_seq[k];
+            }
+            output[position%5][position/5] = output[position%5][position/5]^t;
+            position++;
+        }
+        int num_round;
+        uint64_t output_buf[5][5];
+        for (num_round=0;num_round<24;num_round++) 
+		{
+			//do 24 rounds of Keccak_f
+            Keccak_f(output,output_buf,num_round);
+            Keccak_cpy(output_buf,output);
+        }
+    }
+    
+    
+    
+    uint64_t* result=new uint64_t[output_length];
+    
+    for (i=0;i<output_length;) 
+	{
+		//As in correct type, the length of output is times of 64 and of course time of 8
+		//In each step we output one lane as 64bits=8*8bits
+        for (j=0;j<8;j++) 
+		{
+			//Only consider the last 2
+            result[i]=output[(i/8)%5][(i/8)/5]&0x00000000000000ff;
+            output[(i/8)%5][(i/8)/5]=output[(i/8)%5][(i/8)/5]>>8;   
+            i++;
+        }
+    }
+    return result;
+    
+    
+}
 int main()
 {
 	uint64_t x=0x2000000000000000;
-	
-	cout << hex << Keccak_frot(x,0);
+	uint64_t input[5][5],output[5][5]; 
+	Keccak_f(input,output,1);
+	cout << hex << output[0][0];
 	
 	return 0;
 }
